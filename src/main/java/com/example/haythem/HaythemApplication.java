43e339b24bc36a0f04b27343705e3e876070f56a
Renã Souza <rena.souza@keyrus.com.br>
2019-02-25 10:15:12 -0300

    Initial import

---
+++ src/main/java/com/example/haythem/HaythemApplication.java
@@ -0,0 +1,13 @@
+package com.example.haythem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaythemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaythemApplication.class, args);
+	}
+
+}
